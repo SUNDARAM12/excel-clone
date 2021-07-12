6525ab5bd8e4a82d102a05f2bc11f8d4d85a1e0a
sundaram <sundaram12062000@gmail.com>
2021-07-13 00:56:39 +0530

    till 2nd day of creating excel clone

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ for(let i=1;i<=100;i++){
    rowNumberSection.append(div)
 
 }
+
 let columnTagSection = document.querySelector(".column-tag-section")
 for(let i=0;i<26;i++){
     let asciiCode = 65+i;
@@ -14,4 +15,19 @@ for(let i=0;i<26;i++){
     div.innerText=reqAlphabet;
     div.classList.add("column-tag")
     columnTagSection.append(div);
+}
+let cellTag =  document.querySelector(".cell-section")
+for(let i=1;i<=100;i++){
+let rowDiv=document.createElement("div");
+rowDiv.classList.add("row")
+for(let j=0;j<26;j++){
+let asciiCode=65+j;
+let reqAlphabet=String.fromCharCode(asciiCode);
+let cellAddresss = reqAlphabet+i;
+let cellDiv = document.createElement("div");
+cellDiv.classList.add("cell");
+cellDiv.setAttribute("data-address",cellAddresss)
+rowDiv.append(cellDiv);
+}
+cellTag.append(rowDiv);
 }
