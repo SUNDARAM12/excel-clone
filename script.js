7a02926de8c463f9aca32af1b8350a45265ffd76
sundaram <sundaram12062000@gmail.com>
2021-08-03 12:12:06 +0530

    update3

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let rowNumberSection = document.querySelector(".row-number-section");
+let scd =  document.querySelector(".selected-cell-div");
+let cellTag =  document.querySelector(".cell-section")
+let columnTagSection = document.querySelector(".column-tag-section");
+let lastcell;
+
+cellTag.addEventListener("scroll" , function(e){
+    rowNumberSection.style.transform = `translateY(-${e.currentTarget.scrollTop}px)`;
+     columnTagSection.style.transform = `translateX(-${e.currentTarget.scrollLeft}px)`;
+});
 for(let i=1;i<=100;i++){
    let div = document.createElement("div");
    div.innerText=i; 
@@ -6,8 +15,6 @@ for(let i=1;i<=100;i++){
    rowNumberSection.append(div)
 
 }
-
-let columnTagSection = document.querySelector(".column-tag-section")
 for(let i=0;i<26;i++){
     let asciiCode = 65+i;
     let reqAlphabet=String.fromCharCode(asciiCode);
@@ -16,7 +23,6 @@ for(let i=0;i<26;i++){
     div.classList.add("column-tag")
     columnTagSection.append(div);
 }
-let cellTag =  document.querySelector(".cell-section")
 for(let i=1;i<=100;i++){
 let rowDiv=document.createElement("div");
 rowDiv.classList.add("row")
@@ -25,8 +31,20 @@ let asciiCode=65+j;
 let reqAlphabet=String.fromCharCode(asciiCode);
 let cellAddresss = reqAlphabet+i;
 let cellDiv = document.createElement("div");
+cellDiv.contentEditable=true
 cellDiv.classList.add("cell");
+
+
 cellDiv.setAttribute("data-address",cellAddresss)
+cellDiv.addEventListener("click",function(e){
+    if(lastcell){
+        lastcell.classList.remove("cell-selected")
+    }
+    e.currentTarget.classList.add("cell-selected")
+    lastcell=e.currentTarget
+    let currcell = e.currentTarget.getAttribute("data-address")
+    scd.innerText=currcell;
+});
 rowDiv.append(cellDiv);
 }
 cellTag.append(rowDiv);
